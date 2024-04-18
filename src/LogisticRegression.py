94c4da20b8bce7756a809a1737081b5299444741
Samarth Kumar <144175666+SamarthK1@users.noreply.github.com>
2024-04-17 23:35:45 -0500

    Update Logistic Regression
    - Updated the dataset files used
    - Added training loss curve and more.

--- src/LogisticRegression.py
+++ src/LogisticRegression.py
@@ -1,150 +1,148 @@
-# Importing the necessary libraries
-import pandas as pd
-import numpy as np
-import nltk
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
-from sklearn.model_selection import train_test_split
+
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, precision_score, f1_score, recall_score, confusion_matrix, classification_report
+from sklearn.metrics import classification_report, accuracy_score, log_loss
+from sklearn.linear_model import LogisticRegression, SGDClassifier
+from sklearn.model_selection import GridSearchCV
 import matplotlib.pyplot as plt
-import seaborn as sns
+import numpy as np
+import pandas as pd
+import ast
 
 '''
-Define the helper functions.
+
+Load the data.
+
 '''
 
-# Pre-process the data.
-def preprocess_data(X):
+# Load training data
+train_data = pd.read_csv('train_data.csv')
+X_train_raw = train_data['X_train'].tolist()
+y_train_raw = train_data['y_train'].tolist()
+
+# Load test data
+test_data = pd.read_csv('test_data.csv')
+X_test_raw = test_data['X_test'].tolist()
+y_test_raw = test_data['y_test'].tolist()
+
+#Make it a list
+X_train_raw = [ast.literal_eval(item) if isinstance(item, str) else item for item in X_train_raw]
+X_test_raw = [ast.literal_eval(item) if isinstance(item, str) else item for item in X_test_raw]
+
+# Convert the train and test data into strings for the CountVectorizer.
+train_str = []
+test_str = []
+
+for text in X_train_raw:
+    train_str.append(' '.join(text))
     
-    # Tokenize the text and print the instance to check the loading time.
-    tokenized_texts = []
-    count = 0
-    for text in X:
-        tokenized_texts.append(word_tokenize(text))
-        if count % 100000 == 0:
-            print(count)
-        count += 1
+for text in X_test_raw:
+    test_str.append(' '.join(text))
 
-    # Process the Data, get rid of stop words, lower case the words and stem the tokens.
-    # Stemmer is to reduce the words to their base form.  
-    processed_texts = []
-    stop_words = set(stopwords.words('english'))
-    stemmer = PorterStemmer()
 
-    for text in tokenized_texts:
-        current_tokens = []
-        for token in text:
-            token.lower()
-            if token not in stop_words:
-                stemmer.stem(token)
-                current_tokens.append(token)
-        processed_texts.append(current_tokens)
 
-    # Return the processed texts.
-    return processed_texts
+'''
 
-# Summarize the model performance.
-def model_summary(y_test, y_pred):
+Bag of Words (BoW) Approach
+
+'''
+# Initialize the CountVectorizer, limiting max_features to the top 1000 words.
+vectorizer = CountVectorizer(max_features=1000) 
+
+# Fit the vectorizer on train and test data. Transform the data into BoW matrices.
+train_bow = vectorizer.fit_transform(train_str).toarray()
+test_bow = vectorizer.transform(test_str).toarray()
+
+
+'''
+
+
+Implement and Train the Logistic Regression model
+
+'''
+
+# Initialize the Logistic Regression model
+model = LogisticRegression(max_iter=500)
+
+# Fit the model on the training data
+model.fit(train_bow, y_train_raw)
+
+# Make predictions
+y_pred = model.predict(test_bow)
+
+# Print the classification report
+print(classification_report(y_test_raw, y_pred))
+
+
+'''
+
+Precision Recall Curve
+
+'''
+
+from sklearn.metrics import precision_recall_curve, average_precision_score
+import matplotlib.pyplot as plt
+
+# Predict probabilities for the test data.
+y_probs = model.predict_proba(test_bow)
+
+# Initialize plot colors and figure size.
+plot_colors = ['r', 'b', 'g', 'y', 'm', 'c', 'k']
+plt.figure(figsize=(12, 10))
+
+# Calculate and plot precision-recall curve for each class.
+for i in range(len(model.classes_)):
+
+    # Calculate precision, recall, and average precision.
+    precision, recall, _ = precision_recall_curve((y_test_raw == model.classes_[i]), y_probs[:, i])
+    avg_precision = average_precision_score((y_test_raw == model.classes_[i]), y_probs[:, i])
     
-    # Used accuracy, precision, F1 score, and Recall as the evaluation metrics.
-    accuracy = accuracy_score(y_test, y_pred)
-    precision = precision_score(y_test, y_pred, average='weighted')
-    f1 = f1_score(y_test, y_pred, average='weighted')
-    recall = recall_score(y_test, y_pred, average='weighted')
+    # Plot the curve for the current class.
+    current_color = plot_colors[i % len(plot_colors)]
+    current_label = f'Class {model.classes_[i]} AP={avg_precision:.4f}'
+    plt.plot(recall, precision, color=current_color, label=current_label)
 
-    print('\nLogistic Regression Model Performance:')
-    print(f'Accuracy: {accuracy:.4f} \nPrecision: {precision:.4f} \nF1 Score: {f1:.4f} \nRecall: {recall:.4f}')
+# Plot the curves.
+plt.title('Precision-Recall curve for Logistic Regression')
+plt.xlabel('Recall')
+plt.ylabel('Precision')
+plt.legend(title="Classes", loc='best')
+plt.grid(True)
+plt.show()
 
-    # Print the classification report for more details.
-    print('\nClassification Report (Logistic Regression Classification):')
-    print(classification_report(y_test, y_pred))
 
-# Plot the confusion matrix.
-def plot_confusion_matrix(confusion_matrix):
-    x_labels = range(6)
-    y_labels = range(6)
-    plt.figure(figsize=(12, 9))
-    sns.heatmap(confusion_matrix, annot=True, fmt='d', cmap='Blues', xticklabels=x_labels, yticklabels=y_labels)
-    plt.xlabel('Predicted Labels')
-    plt.ylabel('Actual Labels')
-    plt.title('Confusion Matrix for the Logistic Regression Model')
-    plt.show()
+'''
 
-# Main method
-def main():
+Training Loss Curve
 
-    '''
-    Preprocess the data and split into train and test samples.
-    '''
-    # Path to the gzip-compressed JSON Lines file.
-    data_path = 'Data/data.jsonl.gz'
+'''
+import numpy as np
+from sklearn.linear_model import SGDClassifier
+from sklearn.metrics import log_loss
+import matplotlib.pyplot as plt
 
-    # Load the compressed JSON Lines file into a Pandas DataFrame.
-    data_df = pd.read_json(data_path, lines=True, compression='gzip')
+# Store the training data. 
+X_train = train_bow
+y_train = y_train_raw
 
-    # Assign the features and target variable.
-    X = data_df['text']  
-    y = data_df['label'] 
+# Implement SGDClassifier.
+# loss = 'log' to specify logistic regression.
+model = SGDClassifier(loss='log', learning_rate='constant', eta0=0.01, max_iter=1, warm_start=True, penalty=None)
+losses = []
 
-    # Download the necessary NLTK resources.
-    nltk.download('stopwords')
-    nltk.download('punkt')
-
-    # Call the function to preprocess the data. 
-    processed_texts = preprocess_data(X)
-
-    # Split the data into train and test samples.
-    X_train, X_test, y_train, y_test = train_test_split(processed_texts, y, test_size=0.2, random_state=49)
-
-    '''
-    Implementing the Bag of Words (BoW) model via CountVectorizer to convert textual data into numerical values.
-    '''
-
-    # Convert the train and test data into strings for the CountVectorizer.
-    train_str = []
-    test_str = []
-
-    for text in X_train:
-        train_str.append(' '.join(text))
+# Train the model for 10 epochs.
+for _ in range(10):  
+    model.partial_fit(X_train, y_train, classes=np.unique(y_train))
     
-    for text in X_test:
-        test_str.append(' '.join(text))
+    # Calculate and store the loss.
+    probabilities = model.predict_proba(X_train)
     
-    # Initialize the CountVectorizer, limiting max_features to the top 1000 words.
-    vectorizer = CountVectorizer(max_features=1000) 
+    loss = log_loss(y_train, probabilities)
+    losses.append(loss)
 
-    # Fit the vectorizer on train and test data. Transform the data into BoW matrices.
-    train_bow = vectorizer.fit_transform(train_str).toarray()
-    test_bow = vectorizer.transform(test_str).toarray()
-
-    # Print the shape of the resulting BoW matrices.
-    print('\nBag of Words (BoW) Model:')
-    print('Shape of the BoW Matrix for X_train:', train_bow.shape)
-    print('Shape of the BoW Matrix for X_test:', test_bow.shape)
-
-    '''
-    Implementing the Logistic Regression model.
-    '''
-
-    print('\nTraining the Logistic Regression Model ...\n')
-
-    # Initialize the Logistic Regression model and fit it on the training data.
-    # Set max_iter to 500 to ensure convergence.
-    model = LogisticRegression(max_iter=500)
-    model.fit(train_bow, y_train)
-
-    # Create the predictions.
-    y_pred = model.predict(test_bow)
-
-    # Print the model summary, with detailed evaluation metrics.
-    model_summary(y_test, y_pred)
-
-    # Plot the confusion matrix for a better visualization of the performance.
-    confusion = confusion_matrix(y_test, y_pred)
-    plot_confusion_matrix(confusion)
-    
-if __name__ == '__main__':
-    main()
+# Plot the loss curve.
+plt.figure(figsize=(10, 8))
+plt.plot(losses)
+plt.xlabel('Epoch')
+plt.ylabel('Log Loss')
+plt.title('Training Loss Curve for Logistic Regression')
+plt.show()
