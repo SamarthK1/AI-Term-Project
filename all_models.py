f5d2b081c3426488f6257f80cab4b595900f41ac
CarlosRabat <107375637+CarlosRabat@users.noreply.github.com>
2024-04-30 18:20:19 -0500

    Changes to Overall Results and added MLP to all_models

--- all_models.py
+++ all_models.py
@@ -4,6 +4,11 @@ from sklearn.linear_model import LogisticRegression, SGDClassifier
 import numpy as np
 import pandas as pd
 import ast
+from sklearn.preprocessing import LabelEncoder
+from sklearn.feature_extraction.text import CountVectorizer
+from keras.models import Sequential
+from keras.layers import Dense, Input, Dropout, BatchNormalization
+from keras.regularizers import l2
 
 """
 Script contains all the models to and return their predictions or loss in order to evaluate the results in Overall Results Notebook.
@@ -14,7 +19,67 @@ These are the optimal models for each Machine Learning Algorithm, for detailed i
 
 # Multi-Layer-Perceptron
 def mlp():
-    return
+
+    # Load training data
+    train_data = pd.read_csv("train_data.csv")
+    X_train_raw = train_data["X_train"].tolist()
+    y_train_raw = train_data["y_train"].tolist()
+
+    # Load test data
+    test_data = pd.read_csv("test_data.csv")
+    X_test_raw = test_data["X_test"].tolist()
+    y_test_raw = test_data["y_test"].tolist()
+
+    # Make it a list
+    X_train_raw = [
+        ast.literal_eval(item) if isinstance(item, str) else item
+        for item in X_train_raw
+    ]
+    X_test_raw = [
+        ast.literal_eval(item) if isinstance(item, str) else item for item in X_test_raw
+    ]
+
+    # Join into one big string
+    corpus_train = [" ".join(item) for item in X_train_raw]
+    corpus_test = [" ".join(item) for item in X_test_raw]
+
+    # Initialize the CountVectorizer, limiting max_features to the top 1000 words.
+    vectorizer = CountVectorizer(max_features=1000)
+
+    # Fit the vectorizer on train and test data. Transform the data into BoW matrices.
+    X_train = vectorizer.fit_transform(corpus_train).toarray()
+    X_test = vectorizer.transform(corpus_test).toarray()
+
+    # Encode the taget variables
+    encoder = LabelEncoder()
+    y_train = encoder.fit_transform(y_train_raw)
+    y_test = encoder.fit_transform(y_test_raw)
+
+    # Build the Structure of the Neural Network
+    model = Sequential(
+        [
+            Input(shape=(X_train.shape[1],)),
+            Dense(512, activation="relu", kernel_regularizer=l2(0.001)),
+            Dense(256, activation="relu", kernel_regularizer=l2(0.001)),
+            BatchNormalization(),
+            Dense(128, activation="relu", kernel_regularizer=l2(0.001)),
+            Dense(64, activation="relu", kernel_regularizer=l2(0.001)),
+            Dense(len(set(y_train)), activation="softmax"),
+        ]
+    )
+
+    # Compile and Train the Model
+    model.compile(
+        optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
+    )
+    history = model.fit(
+        X_train, y_train, epochs=10, batch_size=32, validation_split=0.2
+    )
+
+    # Make the Predictions
+    y_pred = model.predict(X_test, batch_size=32)
+
+    return y_pred, history
 
 
 def svm():
