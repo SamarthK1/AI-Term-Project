a51e99c755f558505d2475668596492875dbf9aa
CarlosRabat <107375637+CarlosRabat@users.noreply.github.com>
2024-04-30 14:58:28 -0500

    SVM, all_models and Overall_Results

---
+++ all_models.py
@@ -0,0 +1,27 @@
+"""
+Script contains all the models to and return their predictions or loss in order to evaluate the results in Overall Results Notebook.
+
+These are the optimal models for each Machine Learning Algorithm, for detailed information check each of algorithms notebook.
+"""
+
+
+# Multi-Layer-Perceptron
+def mlp():
+
+    return None
+
+
+def svm():
+    return None
+
+
+def logistic_regression():
+    return None
+
+
+def naive_bayes():
+    return None
+
+
+def decision_trees():
+    return None
